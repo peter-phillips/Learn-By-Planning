8390f49fbe22370a078a707b182978828c5c8e08
kmgarcia23 <kgarci83@calpoly.edu>
2021-05-09 21:45:36 -0700

    Updated Post calls and started routing

---
+++ frontend/src/View.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import Today from './Today';
+import List from './List';
+import Calendar from './Calendar';
+
+function View() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Switch>
+            <Route path="/Calendar">
+              <Calendar />
+            </Route>
+            <Route path="/List">
+              <List />
+            </Route>
+            <Route path="/Today">
+              <Today />
+            </Route>
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default View;

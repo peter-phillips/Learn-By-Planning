83a824c06e90dfa6d1da7d60d1778c8c1dc42e40
Peter Phillips <petermartinphillips@gmail.com>
2021-05-09 20:03:36 -0700

    minor cleanup

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ def create_app():
     users = db.getUsers()
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    # login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     @login_manager.user_loader
--- backend/auth.py
+++ backend/auth.py
@@ -3,7 +3,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from dataBase import DataBase
 from User import User
-import sys
 
 
 auth = Blueprint('auth', __name__)

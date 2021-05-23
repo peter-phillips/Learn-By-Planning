c861874efc34c25a5a0e6d719905a13dc046e7a6
Kevin Garcia <kevinmgarcia1123@gmail.com>
2021-05-23 16:34:40 -0700

    Mostly Finished Routing and started to implement task creation and post

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,51 @@
-/* eslint-disable */
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useState } from 'react';
 import './App.css';
-import Login from './Login';
-import CreateNew from './CreateNew';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Header from './Header';
+import List from './List';
+import Calendar from './Calendar';
+import StartUp from './StartUp';
+import Today from './Today';
+import authContext from './authContext';
 
 function App() {
+  const [auth, setAuth] = useState(false);
+  if (!auth) {
+    return (
+      <authContext.Provider value={{ auth, setAuth }}>
+        <StartUp />
+      </authContext.Provider>
+    );
+  }
   return (
-    <Login />
+    <div>
+      <authContext.Provider value={{ auth, setAuth }}>
+        <BrowserRouter>
+          <Header />
+          <Redirect to="Today" />
+          <main>
+            <Switch>
+              <Route path="/Calendar">
+                <Calendar />
+              </Route>
+              <Route path="/List">
+                <List />
+              </Route>
+              <Route path="/Today">
+                <Today />
+              </Route>
+            </Switch>
+          </main>
+        </BrowserRouter>
+      </authContext.Provider>
+    </div>
   );
 }
 

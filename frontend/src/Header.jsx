94a543180e09e2242f6e72ecf2385d30883a5da6
Kevin Garcia <kevinmgarcia1123@gmail.com>
2021-05-23 23:24:29 -0700

    Finished sending tasks to backend

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,19 +1,20 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import authContext from './authContext';
+// import authContext from './authContext';
 import styles from './Header.module.css';
 
 function Header() {
-  const { setAuth } = useContext(authContext);
-  function logoutUser() {
-    setAuth(false);
-  }
+  // const { setAuth } = useContext(authContext);
+  // function logoutUser() {
+  //   setAuth(false);
+  // }
 
   return (
     <header className={styles.header}>
       <div className={styles.title}>
         <h1 className={styles.name}>LEARN BY PLANNING</h1>
-        <button type="button" onClick={logoutUser} className={styles.logbutton}>Logout</button>
+        {/* <button type="button" onClick={logoutUser}
+        className={styles.logbutton}>Logout</button> */}
       </div>
       <nav id="navBar" className={styles.nav}>
         <NavLink activeClassName={styles.tabActive} className={styles.link} to="/Today">Today</NavLink>

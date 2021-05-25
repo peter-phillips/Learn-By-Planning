585f7b20e8c1a1c3c4b491547e0097a519bff89c
Peter Phillips <petermartinphillips@gmail.com>
2021-05-24 19:25:44 -0700

    Reverted user auth and created a working User

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,35 @@
 from flask import Flask
+from flask_login import LoginManager
 from dataBase import DataBase
 from flask_cors import CORS
 from auth import auth
 from User import User
-import flask_praetorian
 
 db = DataBase()
 users = db.getUsers()
-guard = flask_praetorian.Praetorian()
+login_manager = LoginManager()
 
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
 
-app = Flask(__name__)
-CORS(app)
-app.debug = True
+    app.config['SECRET_KEY'] = '39d76dff06e14b0b9c7bedca6f76e938'
 
-app.config['SECRET_KEY'] = '39d76dff06e14b0b9c7bedca6f76e938'
-app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
-app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
-with app.app_context():
-    guard.init_app(app, User)
+    # login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
-# blueprint for auth routes in our app
-from auth import auth as auth_blueprint
-app.register_blueprint(auth_blueprint)
+    # blueprint for auth routes in our app
+    from auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
-from taskController import task as task_blueprint
-app.register_blueprint(task_blueprint)
+    from taskController import task as task_blueprint
+    app.register_blueprint(task_blueprint)
+
+    return app
+    
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    user = users.find_one({"id" : int(user_id)})
+    return User(user["name"], user["email"], user["password"], int(user_id))
 

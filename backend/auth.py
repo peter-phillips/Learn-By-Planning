b7fce4ffe94700349eaeb54e5ff8507c9430b1b6
Peter Phillips <petermartinphillips@gmail.com>
2021-05-26 08:35:08 -0700

    Minor bug fixes

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user
 from dataBase import DataBase
 from User import User
 import sys

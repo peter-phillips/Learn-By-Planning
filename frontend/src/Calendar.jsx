9b8b023d66171ddfff57a386f49e782380d98ce8
minadedijer <mina-d@live.com>
2021-06-01 22:28:09 -0700

    final

--- frontend/src/Calendar.jsx
+++ frontend/src/Calendar.jsx
@@ -1,41 +1,70 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import axios from 'axios';
 // eslint-disable-next-line react/prefer-stateless-function
+
+function formatEvents(tasks) {
+  const months = {
+    Jan: '01',
+    Feb: '02',
+    Mar: '03',
+    Apr: '04',
+    May: '05',
+    Jun: '06',
+    Jul: '07',
+    Aug: '08',
+    Sep: '09',
+    Oct: '10',
+    Nov: '11',
+    Dec: '12',
+  };
+  const formattedEvents = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const task of tasks) {
+    const title = task.name;
+    const stringStart = JSON.stringify(task.dueDate);
+    const splitStart = stringStart.split(' ');
+    const month = months[splitStart[2]];
+    const color = task.color.hex;
+    let start = '';
+    start += splitStart[3];
+    start += '-';
+    start += month;
+    start += '-';
+    start += splitStart[1];
+    start += 'T';
+    start += splitStart[4];
+    formattedEvents.push({ title, start, color });
+  }
+  return formattedEvents;
+}
+
 export default class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { events: {} };
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get('http://localhost:5000/Calendar');
+      console.log(response.data.tasks);
+      this.setState({ events: formatEvents(response.data.tasks) });
+    } catch (error) {
+      // We're not handling errors. Just logging into the console.
+      console.log(error);
+    }
+  }
+
   render() {
+    const { events } = this.state;
+    console.log(events);
     return (
       <FullCalendar
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
-        green="#154734"
-        events={[
-          {
-            title: 'CPE 307: Midterm 2',
-            start: '2021-05-26T10:30:00',
-            color: 'green',
-          },
-          {
-            title: 'CPE 316: Midterm 2',
-            start: '2021-05-17T12:00:00',
-            color: 'green',
-          },
-          /*
-          {
-            title: 'Click for Google',
-            url: 'http://google.com/',
-            start: '2021-05-28',
-            color: 'green',
-          },
-          */
-        ]}
-        /*
-        events={[
-          { title: 'CSC 307: Midterm 2', date: '2021-05-26' },
-          { title: 'CPE 316: Midterm 2', date: '2021-05-17' },
-        ]}
-        eventColor="#378006"
-        */
+        events={events}
       />
     );
   }
